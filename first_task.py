49391aef7f3194f43b05e0e6b105a1b83fde3cb2
Alibenc <alibencwork@gmail.com>
2025-09-14 16:44:29 +0300

    init comm

---
+++ first_task.py
@@ -0,0 +1,23 @@
+# задание 4
+seconds = 123456
+print('Минуты: ', seconds // 60)
+print('Часы: ', seconds / 60 // 60)
+print('Дни: ', seconds / 60 / 60 // 24)
+
+# 5 задание
+strN = '87'
+n = int(strN)
+
+
+# 6 задание
+x = 228
+y = 337
+
+x, y = y, x
+
+# 7 задание
+number = 233
+if number % 2 == 0:
+  print('четное')
+else:
+  print('нечетное')
